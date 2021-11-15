fe691ecfa8ab36b5daed9d8321290da8061a01c3
ezez-refer <jjn00712@gmail.com>
2021-11-16 01:31:33 +0900

    class 3

--- CLASS 3/1로 만들기_1463.py
+++ CLASS 3/1로 만들기_1463.py
@@ -1,31 +1,13 @@
 X = int(input())
 
-cal_dict = {}
-num_2 = 2
-num_3 = 3
+res = [0] * (X + 1)
 
-num = 1
-while num_2 < 10**6 and num_3 < 10**6:
-    num_2 = num_2 * num
-    num_3 = num_3 * num
+for i in range(2, X + 1):
+    res[i] = res[i - 1] + 1
 
-    if num_2 > 10**6:
-        break
-    if num_3 > 10**6:
-        break
+    if i % 2 == 0:
+        res[i] = min(res[i], res[i // 2] + 1)
+    if i % 3 == 0:
+        res[i] = min(res[i], res[i // 3] + 1)
 
-    if not num_2 in cal_dict:
-        cal_dict[num_2] = num
-    if not num_3 in cal_dict:
-        cal_dict[num_3] = num
-    
-    num += 1
-
-count = 0
-while X > 1:
-    if X in cal_dict:
-        print(cal_dict[X] + count)
-        break
-    
-    X -= 1
-    count += 1
+print(res[X])
