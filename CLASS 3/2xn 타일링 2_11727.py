cba67f2e4feceab11862853aed7dfad4444263a7
ezez-refer <jjn00712@gmail.com>
2021-11-26 18:57:46 +0900

    class 3

---
+++ CLASS 3/2xn 타일링 2_11727.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+case = [1, 3]
+for i in range(2, n):
+    case.append(case[i-2] * 2 + case[i-1])
+
+if n < 2:
+    print(case[0]%10007)
+elif n < 3:
+    print(case[1]%10007)
+else:
+    print(case[-1]%10007)
