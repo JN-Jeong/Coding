95f098762f0e4425f01d43eebae7d5c58494c127
ezez-refer <jjn00712@gmail.com>
2021-12-03 18:34:17 +0900

    revision

--- CLASS 3/나는야 포켓몬 마스터 이다솜_1620.py
+++ CLASS 3/나는야 포켓몬 마스터 이다솜_1620.py
@@ -3,17 +3,15 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 
 pokemon_dict = {}
+rev_pokemon_dict = {}
 for i in range(1, N+1):
     pokemon = input()
     pokemon_dict[i] = pokemon
-
-rev_pokemon_dict = {}
-for key, value in pokemon_dict.items():
-    rev_pokemon_dict[value] = key
+    rev_pokemon_dict[pokemon] = i
 
 for i in range(M):
     question = input()
-    if question.upper() == question.lower(): # 숫자인지 확인
+    if question.upper() == question.lower(): # 숫자
         print(pokemon_dict[int(question)])
-    else: #
+    else: # 문자
         print(rev_pokemon_dict[question])
