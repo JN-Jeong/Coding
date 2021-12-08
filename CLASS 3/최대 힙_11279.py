076484b268bdbf07320bfb43ba3a1a20a7e5c62a
ezez-refer <jjn00712@gmail.com>
2021-12-08 18:21:34 +0900

    class 3

--- CLASS 3/최대 힙_11279.py
+++ CLASS 3/최대 힙_11279.py
@@ -1,12 +1,15 @@
-# 미완료
+# pypy3로 제출
 import heapq
 
 N = int(input())
 
-heap = []
-heapq.heapify(heap) # 최소 힙
+num_list = []
 for _ in range(N):
-    x = int(input())
+    num_list.append(int(input()))
+
+heap = []
+heapq.heapify(heap) # 최소 힙 구축
+for x in num_list:
     if x == 0:
         if len(heap) > 0:
             print("출력 : ", abs(heapq.heappop(heap))) # 절댓값을 통해 음수로 구성된 원소를 양수로 출력하면 최소값이 최대값으로 출력된다
