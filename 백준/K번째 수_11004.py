9af2acd2ecd27391eee6b4083ddbbfbb3de4d897
ezez-refer <jjn00712@gmail.com>
2022-06-02 19:07:25 +0900

    백준

---
+++ 백준/K번째 수_11004.py
@@ -0,0 +1,38 @@
+# def merge_sort(array):
+#     if len(array) <= 1:
+#         return array
+
+#     mid = len(array) // 2
+#     left = merge_sort(array[:mid])
+#     right = merge_sort(array[mid:])
+
+#     i, j, k = 0, 0, 0
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             array[k] = left[i]
+#             i += 1
+#         else:
+#             array[k] = right[j]
+#             j += 1
+#         k += 1
+    
+#     if i == len(left):
+#         while j < len(right):
+#             array[k] = right[j]
+#             j += 1
+#             k += 1
+#     elif j == len(right):
+#         while i < len(left):
+#             array[k] = left[i]
+#             i += 1
+#             k += 1
+
+#     return array
+
+N, K = map(int, input().split())
+array = list(map(int, input().split()))
+
+# array = merge_sort(array)
+array.sort()
+print(array)
+print(array[K-1])
