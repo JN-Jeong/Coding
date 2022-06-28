373d71450f79cbd645b8679906265ae2fdbc4c1b
ezez-refer <jjn00712@gmail.com>
2022-06-28 20:08:48 +0900

    백준

---
+++ 백준/숫자 카드_10815.py
@@ -0,0 +1,15 @@
+N = int(input())
+N_list = list(map(int, input().split()))
+M = int(input())
+M_list = list(map(int, input().split()))
+
+N_dict = {}
+for n in N_list:
+    if n not in N_dict:
+        N_dict[n] = 1
+
+for m in M_list:
+    if m in N_dict:
+        print(N_dict[m], end = ' ')
+    else:
+        print(0, end = ' ')
