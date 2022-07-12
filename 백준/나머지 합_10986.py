69ea61930bfa2491b7fbd879ac8b641b8efd8847
ezez-refer <jjn00712@gmail.com>
2022-07-12 19:10:28 +0900

    백준

---
+++ 백준/나머지 합_10986.py
@@ -0,0 +1,46 @@
+'''
+참고
+https://zoosso.tistory.com/550
+
+5 3
+1 2 3 1 2
+
+7
+-------------
+'''
+
+# N, M = map(int, input().split())
+# A = [0] + list(map(int, input().split())) # 런타임 에러(NameError) 발생함, 왜지?
+
+# memo = [0] * M
+
+# for i in range(N):
+#     A[i+1] += A[i]
+#     memo[A[i] % M] += 1
+
+# print(A)
+# print(memo)
+
+# answer = memo[0]
+# for i in range(len(memo)):
+#     answer += memo[i] * (memo[i] - 1) // 2
+
+# print(answer)
+
+N, M = map(int, input().split())
+A = list(map(int, input().split())) + [0]
+
+memo = [0] * M
+
+for i in range(N):
+    A[i] += A[i - 1]
+    memo[A[i] % M] += 1
+
+print(A)
+print(memo)
+
+answer = memo[0]
+for i in range(len(memo)):
+    answer += memo[i] * (memo[i] - 1) // 2
+
+print(answer)
