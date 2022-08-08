4be34c1bc883b9e04029e3395a67918fdb20c078
ezez-refer <jjn00712@gmail.com>
2022-08-08 19:51:47 +0900

    백준

---
+++ 백준/가장 긴 증가하는 부분 수열_11053.py
@@ -0,0 +1,26 @@
+"""
+수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열 길이 구하기
+int 반환
+DP 문제라고 함
+index 필요 없음
+"""
+
+def solve():
+    A_size = int(input())
+    A = list(map(int, input().split()))
+    memo = [1] * A_size
+    print(memo)
+
+    for i in range(A_size):
+        for j in range(i):
+            if A[i] > A[j]:
+                if memo[i] <= memo[j]:
+                    memo[i] = memo[j] + 1
+                print(i,j)
+                print("memo : ", memo[i], memo[j])
+                
+    
+    return max(memo)
+
+if __name__ == "__main__":
+    print(solve())
