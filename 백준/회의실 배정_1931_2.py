64499290bd35d327d1072d5c70b173d6c27d3fdc
ezez-refer <jjn00712@gmail.com>
2022-08-14 14:31:22 +0900

    백준

--- 백준/회의실 배정_1931_2.py
+++ 백준/회의실 배정_1931_2.py
@@ -5,4 +5,14 @@ for _ in range(N):
     rooms.append(list(map(int, input().split())))
 
 rooms.sort()
+rooms.sort(key=lambda x:x[1])
 print(rooms)
+
+answer = 1
+temp = rooms[0][1]
+for i in range(1, len(rooms)):
+    if temp <= rooms[i][0]:
+        temp = rooms[i][1]
+        answer += 1
+
+print(answer)
