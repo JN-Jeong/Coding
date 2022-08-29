94161d5b9f7a1a05c364140c34483f8531639391
ezez-refer <jjn00712@gmail.com>
2022-08-29 19:51:51 +0900

    class 3

--- CLASS 3/1,2,3 더하기_9095.py
+++ CLASS 3/1,2,3 더하기_9095.py
@@ -1,4 +1,4 @@
-'''
+"""
 문제
 정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
 
@@ -16,14 +16,14 @@
 
 출력
 각 테스트 케이스마다, n을 1, 2, 3의 합으로 나타내는 방법의 수를 출력한다.
-'''
+"""
 
 T = int(input())
 
-case = [1,2,4]
+case = [1, 2, 4]
 for i in range(3, 11):
-    case.append(case[i-3] + case[i-2] + case[i-1])
+    case.append(case[i - 3] + case[i - 2] + case[i - 1])
 
 for _ in range(T):
     n = int(input())
-    print(case[n-1])
+    print(case[n - 1])
