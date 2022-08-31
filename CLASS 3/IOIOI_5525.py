e0f288441c17478462e4fbcc75624df942aad87f
ezez-refer <jjn00712@gmail.com>
2022-08-31 17:57:50 +0900

    class 3

--- CLASS 3/IOIOI_5525.py
+++ CLASS 3/IOIOI_5525.py
@@ -7,8 +7,8 @@ S = sys.stdin.readline().strip()
 num = 0
 count = 0
 i = 1
-while i < len(S)-1:
-    if S[i-1] == 'I' and S[i] == 'O' and S[i+1] == 'I':
+while i < len(S) - 1:
+    if S[i - 1] == "I" and S[i] == "O" and S[i + 1] == "I":
         count += 1
         if count >= N:
             num += 1
@@ -16,5 +16,5 @@ while i < len(S)-1:
     else:
         count = 0
         i += 1
-    
+
 print(num)
