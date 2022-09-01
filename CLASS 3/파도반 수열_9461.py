63600626b65f4317bc1ef4cda500931c987373ae
ezez-refer <jjn00712@gmail.com>
2022-09-01 17:02:46 +0900

    class 3

--- CLASS 3/파도반 수열_9461.py
+++ CLASS 3/파도반 수열_9461.py
@@ -2,9 +2,9 @@ T = int(input())
 
 for _ in range(T):
     N = int(input())
-    tri = [1,1,1,2,2]
+    tri = [1, 1, 1, 2, 2]
     for i in range(5, N):
-        tri.append(tri[i-3] + tri[i-2])
+        tri.append(tri[i - 3] + tri[i - 2])
     if N < 4:
         print(1)
     elif N < 6:
