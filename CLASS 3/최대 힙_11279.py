c0fcf496d90ffaf40ddce66761d466c622a8f157
ezez-refer <jjn00712@gmail.com>
2022-09-06 18:00:40 +0900

    class 3

--- CLASS 3/최대 힙_11279.py
+++ CLASS 3/최대 힙_11279.py
@@ -7,13 +7,15 @@ num_list = []
 for _ in range(N):
     num_list.append(int(input()))
 
-heap = []
-heapq.heapify(heap) # 최소 힙 구축
+heap: list = []
+heapq.heapify(heap)  # 최소 힙 구축
 for x in num_list:
     if x == 0:
         if len(heap) > 0:
-            print("출력 : ", abs(heapq.heappop(heap))) # 절댓값을 통해 음수로 구성된 원소를 양수로 출력하면 최소값이 최대값으로 출력된다
+            print(
+                "출력 : ", abs(heapq.heappop(heap))
+            )  # 절댓값을 통해 음수로 구성된 원소를 양수로 출력하면 최소값이 최대값으로 출력된다
         else:
             print("출력 : ", 0)
     else:
-        heapq.heappush(heap, -x) # 최소 힙으로 구현된 heapq에 - 를 붙여 음수로 원소를 구성한다
+        heapq.heappush(heap, -x)  # 최소 힙으로 구현된 heapq에 - 를 붙여 음수로 원소를 구성한다
