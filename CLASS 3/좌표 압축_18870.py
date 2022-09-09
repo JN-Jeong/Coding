af404c44cb9da3c898be6fa272705a9fff165191
ezez-refer <jjn00712@gmail.com>
2022-09-09 17:57:09 +0900

    class 3

--- CLASS 3/좌표 압축_18870.py
+++ CLASS 3/좌표 압축_18870.py
@@ -3,10 +3,10 @@ X = list(map(int, input().split()))
 
 set_X = {}
 idx = 0
-for i in sorted(X): # 정렬된 X 리스트의 변수들에 오름차순으로 번호를 부여한다
+for i in sorted(X):  # 정렬된 X 리스트의 변수들에 오름차순으로 번호를 부여한다
     if not i in set_X:
         set_X[i] = idx
         idx += 1
 
 for i in X:
-    print(set_X[i], end=' ')
+    print(set_X[i], end=" ")
