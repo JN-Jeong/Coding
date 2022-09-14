2c14bfd2d51d9ec9be3a6c844a3fbef55644886b
ezez-refer <jjn00712@gmail.com>
2022-09-14 18:00:30 +0900

    class 3

--- CLASS 3/팩토리얼 0의 개수_1676.py
+++ CLASS 3/팩토리얼 0의 개수_1676.py
@@ -1,20 +1,23 @@
 N = int(input())
 
+
 def facto(num):
     if num < 2:
         return num
-    
-    return num * facto(num-1)
+
+    return num * facto(num - 1)
+
 
 def zero_num(num):
     res = 0
     str_num = str(num)
     for i in range(1, len(str_num)):
-        if str_num[-i] == '0':
+        if str_num[-i] == "0":
             res += 1
         else:
             return res
     return res
+
 
 N_facto = facto(N)
 print(zero_num(N_facto))
