e7a5418ec9ecc7648acbe586f7144b48e7a88e72
ezez-refer <jjn00712@gmail.com>
2022-09-30 18:27:57 +0900

    programmers

--- programmers/주식가격.py
+++ programmers/주식가격.py
@@ -1,32 +1,13 @@
-# def solution(prices):
-#     answer = [0] * len(prices)
-    
-#     # 각 주식이 가격이 떨어지지 않은 기간이 몇 초인지 list로 반환
-
-#     time = 0
-#     price = 0 # 현재 금액
-#     while time < len(prices): # 
-#         price = prices[time]
-#         time += 1
-#         print(price, time)
-#         for i in range(time):
-#             if prices[i] <= price: # 가격이 떨어지지 않았다면
-#                 answer[i] += 1
-#             else: # 가격이 떨어졌다면
-#                 prices[i] = 10001
-    
-#     print(answer)
-#     return answer
-
+"""
+각 주식의 가격이 떨어지지 않은 기간이 몇 초인지 list로 반환
+"""
 
 from collections import deque
 
 
 def solution(prices):
     answer = []
-    
-    # 각 주식의 가격이 떨어지지 않은 기간이 몇 초인지 list로 반환
-    
+
     q = deque(prices)
 
     while q:
@@ -38,6 +19,16 @@ def solution(prices):
             if price > i:
                 break
         answer.append(time)
-                
-    print(answer)
+
     return answer
+
+
+prices = [
+    [1, 2, 3, 2, 3],
+]
+result = [
+    [4, 3, 1, 1, 0],
+]
+
+for price in prices:
+    print(solution(price))
