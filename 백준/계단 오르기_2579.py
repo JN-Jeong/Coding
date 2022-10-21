2aeb125dd63f6678edda1cc2403d9042ef407456
ezez-refer <jjn00712@gmail.com>
2022-10-21 18:57:37 +0900

    백준

--- 백준/계단 오르기_2579.py
+++ 백준/계단 오르기_2579.py
@@ -9,7 +9,8 @@ if n > 1:
     score[1] = score[0] + stairs[1]
 if n > 2:
     for i in range(2, n):
-        print(i-2, i-3, score)
-        score[i] = max(score[i-2], score[i-3] + stairs[i-1]) + stairs[i]    # i가 2일 때 score[i-3]이면 score[-1]이고 값이 0임, + stairs[i] = 마지막 도착 계단
+        print(i - 2, i - 3, score)
+        # i가 2일 때 score[i-3]이면 score[-1]이고 값이 0임, + stairs[i] = 마지막 도착 계단
+        score[i] = max(score[i - 2], score[i - 3] + stairs[i - 1]) + stairs[i]
 
 print(score[-1])
