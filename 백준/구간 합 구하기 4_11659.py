fca392eda3ef48c6ef9db56997f3a4e76ff637f5
ezez-refer <jjn00712@gmail.com>
2022-10-26 19:05:08 +0900

    백준

--- 백준/구간 합 구하기 4_11659.py
+++ 백준/구간 합 구하기 4_11659.py
@@ -1,4 +1,4 @@
-'''
+"""
 5 3
 5 4 3 2 1
 1 3
@@ -9,19 +9,19 @@
 9
 1
 ---------------
-'''
+"""
 
 import sys
+
 N, M = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 print(nums)
 
-temp = [0] * (N+1)
+temp = [0] * (N + 1)
 for i in range(1, len(temp)):
-    temp[i] += temp[i-1] + nums[i-1]
+    temp[i] += temp[i - 1] + nums[i - 1]
 print(temp)
 
 for _ in range(M):
     n1, n2 = map(int, sys.stdin.readline().split())
-    print(temp[n2] - temp[n1-1])
-    
+    print(temp[n2] - temp[n1 - 1])
