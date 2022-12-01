677ca04c93f0d1ce7b5d2638d021ed36af751b12
ezez-refer <jjn00712@gmail.com>
2022-12-01 19:00:20 +0900

    SWEA

---
+++ SW Expert/거미_14695/거미 permu X.py
@@ -0,0 +1,35 @@
+def solution():
+    answer = "NIE"
+    N = int(input())
+    flies = []
+
+    for _ in range(N):
+        flies.append(list(map(int, input().split())))
+    print(flies)
+
+    for i in range(len(flies)):
+        for j in range(len(flies)):
+            if i == j:
+                continue
+            normal_vector = cross_product(flies[i], flies[j])
+            print("@ : ", normal_vector)
+
+            if normal_vector in flies:
+                return "TAK"
+
+    return answer
+
+
+def cross_product(vector1, vector2):  # 법선 벡터 (normal vector)
+    return [
+        vector1[1] * vector2[2] - vector1[2] * vector2[1],
+        vector1[2] * vector2[0] - vector1[0] * vector2[2],
+        vector1[0] * vector2[1] - vector1[1] * vector2[0],
+    ]
+
+
+if __name__ == "__main__":
+    TC = int(input())
+
+    for i in range(TC):
+        print(f"#{i+1} {solution()}")
