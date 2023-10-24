814044e673a5d95356826228cdd09ea1f7c84037
JN-Jeong <jjn00712@gmail.com>
2023-10-24 13:27:17 +0900

    programmers

--- programmers/완전탐색/전력망을 둘로 나누기.py
+++ programmers/완전탐색/전력망을 둘로 나누기.py
@@ -1,61 +1,52 @@
 """
-전선 하나를 끊음 -> permutations으로 n-1개?
+하나로 연결되어 있는 전력망
+전선을 하나 끊어서 하나였던 전력망을 둘로 나눠야 함
+
+전력망이 두 개이기 때문에 송전탑 하나를 시작으로 연결된 송전탑을 모두 방문하여 한 쪽 전력망의 송전탑 개수를 세면 양쪽 전력망의 송전탑 개수를 알 수 있음
+연결되어 있는 전선을 하나씩 끊으면서 완전탐색으로 양쪽 송전탑 개수의 차이를 업데이트 해주어서 차이가 가장 적은 값을 반환
 """
 
 
 from collections import deque
-from itertools import permutations
 
 
 def solution(n, wires):
     answer = float("inf")
 
-    # for wire in wires:
-    #     print(wire)
-
-    ids = list(map(list, permutations(range(1, len(wires) + 1), len(wires) - 1)))
-    # print(ids)
-
-    for idx in ids:
-        # wire = [wires[i] for i in idx]
-        # print(wire)
-        answer = min(answer, (search(make_graph(n, wires, idx))))
+    for wire in wires:
+        graph = make_graph(n, wires)
+        # print(graph)
+        temp = search(graph, wire)
+        answer = min(answer, abs((n - temp) - temp))
 
     return answer
 
 
-def search(graph):
-
+def search(graph, del_wire):
     visited = [0] * len(graph)
-    cnt = 0
-    result = 0
+    visited[del_wire[0]] = 1  # del_wire[0] : 전선 끊기 (방문하지 않도록 해줌)
 
-    for idx in range(1, len(graph)):
-        q = deque()
-        q.append(idx)
-        net = 0
+    q = deque()
+    q.append(graph[del_wire[0]])
+    net = 1
 
-        if visited[idx] == 1:
-            continue
+    while q:
+        connect = q.popleft()
 
-        while q:
-            i = q.popleft()
-            for j in range(1, len(graph)):
-                if i != j and graph[i][j] == 1 and visited[j] == 0:
-                    visited[j] = 1
-                    q.append(j)
-                    net += 1
+        for i in range(1, len(connect)):
+            if connect[i] == 1 and visited[i] == 0 and i != del_wire[1]:  # del_wire[1] : 전선 끊기 (방문하지 않도록 해줌, 큐에 추가 안함)
+                visited[i] = 1
+                q.append(graph[i])
+                net += 1
 
-        result = abs(result - net)
-        cnt += 1
-
-    return result
+    print("# : ", net)
+    return net
 
 
-def make_graph(n, wire, idx):
+def make_graph(n, wires):
     graph = [[0] * (n + 1) for _ in range(n + 1)]
-    for i in idx:
-        l_wire, r_wire = wire[i - 1]
+    for wire in wires:
+        l_wire, r_wire = wire
         graph[l_wire][r_wire] = graph[r_wire][l_wire] = 1
 
     return graph
