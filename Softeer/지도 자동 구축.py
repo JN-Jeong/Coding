f6187854d36983ed6adbe448abb059f6266635ab
JN-Jeong <jjn00712@gmail.com>
2023-11-22 11:46:57 +0900

    Softeer

---
+++ Softeer/지도 자동 구축.py
@@ -0,0 +1,15 @@
+"""
+https://softeer.ai/practice/6280
+지도 자동 구축
+"""
+
+import sys
+
+
+def solution():
+    n = int(input())
+    return (2**n + 1) ** 2
+
+
+if __name__ == "__main__":
+    print(solution())
